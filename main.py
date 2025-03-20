161c84a7d7b8d305977b3d0582080646f833cdc8
DrGorge <drobeng68@gmail.com>
2025-03-20 21:30:40 +0300

    add cacle code

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_WIDTH = 800
 SCREEN_HIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGHT))
 pygame.display.set_caption('Tir Game')
-icon = pygame.image.load("img/icon1.jpg")
+icon = pygame.image.load('img/icon.jpg')
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.png")
+target_img = pygame.image.load('img/icon1.png')
 target_width = 80
 target_hight = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
@@ -23,13 +23,21 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-        pass
+        screen.fill(color)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_hight:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HIGHT - target_hight)
 
 
 
 
-
-
+screen.blit(target_img, (target_x, target_y))
+pygame.display.update()
 
 
 pygame.exit()
